8e03a60f9749f8aebf9695e1f239e88548fbc655
Helen <howareyouhelen@hotmail.com>
2017-07-19 00:46:54 +0000

    up and down for migration file, added table

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+const settings = require("./settings");
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+}
